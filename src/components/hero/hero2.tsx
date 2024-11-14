b88ee46f9646eaf670cf1df43a5cf4760aa48044
Eyerusalem Abate <eyerusabate2@gmail.com>
2024-11-14 11:12:32 +0300

    change the hero section

--- src/components/hero/hero2.tsx
+++ src/components/hero/hero2.tsx
@@ -41,17 +41,17 @@ const Hero2 = () => {
       <div className="bubble bubble-5 bg-blue-300 opacity-20 animate-float w-20 h-20 rounded-full absolute top-[2%] left-[5%] animate-delay-4s shadow-2xl"></div>
 
       <div className="h-screen flex  text-[#323232] z-10 relative">
-        <div className="flex flex-col gap-2 sm:w-[60%] pt-16 md:ml-24 max-md:ml-5 ">
+        <div className="flex flex-col gap-2 sm:w-[60%] pt-16 md:ml-24 max-md:ml-5">
           <h1 className="self-start text-[13pt] font px-5 text-white py-1 rounded-lg bg-[#433ed5]">
             Hi all! 😊 I’m
           </h1>
-          <h1 className="text-[45pt] max-lg:text-[25pt]  font-extrabold text-[#241fa4]">
+          <h1 className="text-[45pt] max-lg:text-[30pt]  font-extrabold text-[#241fa4]">
             Eyerusalem Abate
           </h1>
-          <h1 className="font-bold text-[20pt] max-lg:text-[14pt]">
+          <h1 className="font-bold text-[20pt] max-lg:text-[18pt]">
             <strong className="text-[#241fa4]">UI/UX designer</strong> & Frontend Developer
           </h1>
-          <p className="text-[14pt] font-[510] md:w-[80%] max-md:text-center max-md:text-[10pt] max-md:text-[450]">
+          <p className="text-[14pt] font-[510] md:w-[80%] max-md:text-center max-md:text-[450]">
             Passionate about creating user-centered experiences that solve
             real-world problems through intuitive and impactful design, focused
             on enhancing daily life and delivering lasting value.

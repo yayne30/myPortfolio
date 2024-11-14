a6d569592c96c0b8a375f63ca0ccef8a0117b405
Eyerusalem Abate <eyerusabate2@gmail.com>
2024-11-14 10:44:17 +0300

    add scroll animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,42 @@
-import './App.css'
-import ContactMe from './components/contactme/contact'
-import Rules from './components/designrules/rules'
-import Header from './components/header/header'
-// import Hero from './components/hero/hero'
-import Hero2 from './components/hero/hero2'
-import Projects  from './components/projects/projects'
-import ToolKit from './components/toolkit/toolkit'
+import 'aos/dist/aos.css';  // Import AOS styles
+import AOS  from 'aos'
+import { useEffect } from 'react';
+import Header from './components/header/header';
+import Hero2 from './components/hero/hero2';
+import Projects from './components/projects/projects';
+import Rules from './components/designrules/rules';
+import ToolKit from './components/toolkit/toolkit';
+import ContactMe from './components/contactme/contact';
 
 function App() {
-
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,  // Animation duration in milliseconds
+      once: false       // Whether animation should happen only once
+    });
+  }, []);
+  
   return (
     <div className='bg-moving-gradient'>
-    <Header />
-    <div>
-      <section id='about'>
-        <Hero2 />
-      </section>
-      <section id='projects'>
-        <Projects />
-      </section>
-      <section id='projects'>
-        <Rules />
-      </section>
-      <section id='projects'>
-        <ToolKit />
-      </section>
-      <section id='projects'>
-        <ContactMe />
-      </section>
+      <Header />
+      <div>
+        <section id='about' data-aos="fade-up">
+          <Hero2 />
+        </section>
+        <section id='projects' data-aos="fade-up">
+          <Projects />
+        </section>
+        <section id='rules' data-aos="fade-up">
+          <Rules />
+        </section>
+        <section id='toolKit' data-aos="fade-up">
+          <ToolKit />
+        </section>
+        <section id='contactMe' data-aos="fade-up">
+          <ContactMe />
+        </section>
+      </div>
     </div>
-
-
-   
-      
-    </div>
-  )
+  );
 }
-
-export default App
+export default App;
